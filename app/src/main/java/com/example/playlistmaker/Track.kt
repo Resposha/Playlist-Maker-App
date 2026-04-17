3d5f916ca0a40f11780fc8cecaf909348b173fc9
Resposha <anyashnik@gmail.com>
2026-04-18 00:22:03 +0300

    Добавить выполнение поисковых запросов на экран поиска

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,8 +1,8 @@
 package com.example.playlistmaker
 
 data class Track(
-    val trackName: String, // Название композиции
-    val artistName: String, // Имя исполнителя
-    val trackTime: String, // Продолжительность трека
-    val artworkUrl100: String // Ссылка на изображение обложки
+    val trackName: String, // название композиции
+    val artistName: String, // имя исполнителя
+    val trackTimeMillis: String, // продолжительность трека
+    val artworkUrl100: String // ссылка на изображение обложки
 )
