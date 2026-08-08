76c24a7de55adc8dd8d3e127d5e344e89027bc84
Resposha <anyashnik@gmail.com>
2026-08-08 11:28:45 +0300

    Реализовать паттерн навигации с использованием BottomNavigationView

--- app/src/main/java/com/example/playlistmaker/main/ui/RootActivity.kt
+++ app/src/main/java/com/example/playlistmaker/main/ui/RootActivity.kt
@@ -1,11 +1,15 @@
 package com.example.playlistmaker.main.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
@@ -23,5 +27,21 @@ class RootActivity : AppCompatActivity() {
             view.updatePadding(top = statusBar.top)
             insets
         }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.root_fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.playerFragment -> {
+                    binding.rootFragmentBottomNavigationView.visibility = View.GONE
+                }
+                else -> {
+                    binding.rootFragmentBottomNavigationView.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        binding.rootFragmentBottomNavigationView.setupWithNavController(navController)
     }
 }
